49845160804f0176d52f8eae3856871275121f47
Kevin Herzig <kevin@herzig.net>
2017-02-08 12:51:33 -0500

    Added link to GitHub

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 
+//  Copyright 2017 Kevin Herzig
+
+//      This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 var express = require('express');
 var path = require('path');
 var ws = require("sockjs");
@@ -221,4 +238,3 @@ process.on('SIGTERM', gracefulShutdown);
 
 // listen for INT signal e.g. Ctrl-C
 process.on('SIGINT', gracefulShutdown);
-
