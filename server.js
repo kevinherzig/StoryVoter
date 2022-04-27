305b6f34636b2e3e95e9ad22620f14500467eb10
Kevin Herzig <kevin@herzig.net>
2022-04-27 09:19:28 -0400

    Output loggin to console

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const logger = winston.createLogger({
     //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console(),
   ],
 });
 
